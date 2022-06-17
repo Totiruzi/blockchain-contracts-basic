42b8005c753e316abe0bced4750af17dcf053b3c
Totiruzi <totiruzi@yahoo.com>
2022-06-17 05:24:44 +0300

    Install ethers, connect provider and private key

--- deploy.js
+++ deploy.js
@@ -1,4 +1,12 @@
-async function main() {}
+const ethers = require('ethers');
+
+async function main() {
+  // compile contracts in our code
+  // compile them seperately
+  // http://127.0.0.1:7545
+  const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545');
+  const wallet = new ethers.Wallet('0c35cbff8ae9aebf5fd66f86d73b6de76d2b54b8145cd013996113aea05d5c54', provider);
+}
 
 main()
   .then(() => process.exit(0))
