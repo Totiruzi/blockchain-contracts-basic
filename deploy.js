ee3a8ac5e592c88f812a65ba1078098f10c54cb4
Totiruzi <totiruzi@yahoo.com>
2022-07-01 00:50:49 +0300

    You are doing great

--- deploy.js
+++ deploy.js
@@ -9,12 +9,21 @@ async function main() {
   const wallet = new ethers.Wallet('0c35cbff8ae9aebf5fd66f86d73b6de76d2b54b8145cd013996113aea05d5c54', provider);
   const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8');
   const bytecode = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.bin', 'utf8');
+  
   // A contract factory is a JavaScript object that is able to deploy a contract on the blockchain.
   const contractFactory = new ethers.ContractFactory(abi, bytecode, wallet);
+
+  // Deploy a contract
   console.log('Please wait while the contract is being deployed...'); 
   const contract = await contractFactory.deploy();
   console.log(contract);
+
+  // Get the address of the contract
   console.log('Contract deployed at:', contract.address);
+
+  // Get the transaction hash of the deployment
+  const transactionRecipt = await contract.deployTransaction.wait(1);
+  console.log(transactionRecipt);
 }
 
 main()
